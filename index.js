02d1540ef6137bed460d85c565eef5604553eac7
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-18 17:44:53 +0300

    add cors

--- index.js
+++ index.js
@@ -1,96 +1,100 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-morgan.token('body', (request) => {
-    return request.method === 'POST' ? JSON.stringify(request.body) : ''
-})
+morgan.token("body", (request) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : "";
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+app.use(cors());
 
 let persons = [
-    { 
-        id: "1",
-        name: "Arto Hellas", 
-        number: "040-123456"
-      },
-      { 
-        id: "2",
-        name: "Ada Lovelace", 
-        number: "39-44-5323523"
-      },
-      { 
-        id: "3",
-        name: "Dan Abramov", 
-        number: "12-43-234345"
-      },
-      { 
-        id: "4",
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122"
-      }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has started for ${persons.length} people </p> <p>${new Date().toLocaleString()}</p>`)
-})
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has started for ${persons.length} people </p> <p>${new Date().toLocaleString()}</p>`,
+  );
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
 
 const generateId = () => {
-    const maxId = persons.length > 0 
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-    return(maxId + 1)
-}
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/persons', (request, response) => {
-    const {name, number} = request.body
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
 
-    if (!name){
-        return response.status(400).json({error: 'Name Missing'})
-    } 
-    if (!number) {
-        return response.status(400).json({error: 'Number Missing'})
-    }
-    if (persons.some(person => person.name === name)){
-        return response.status(400).json({error: 'Name must be unique'})
-    }
+  if (!name) {
+    return response.status(400).json({ error: "Name Missing" });
+  }
+  if (!number) {
+    return response.status(400).json({ error: "Number Missing" });
+  }
+  if (persons.some((person) => person.name === name)) {
+    return response.status(400).json({ error: "Name must be unique" });
+  }
 
-    const person = {
-        name,
-        number,
-        id: generateId()
-    }
+  const person = {
+    name,
+    number,
+    id: generateId(),
+  };
 
-    persons = persons.concat(person)
+  persons = persons.concat(person);
 
-    response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = 3001
-app.listen(PORT, ()=> {
-    console.log(`App running in port ${PORT}`);
-})
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`App running in port ${PORT}`);
+});
