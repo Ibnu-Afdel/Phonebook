b0a61e9c1bf0c3e90699bc9fb168d8a0820f23b2
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-19 21:19:25 +0300

    better error handling, edit the database data

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformated id" });
+  }
+  next(error);
+};
+
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
@@ -51,24 +59,46 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has started for ${
-      persons.length
-    } people </p> <p>${new Date().toLocaleString()}</p>`
-  );
+  Person.countDocuments({})
+    .then((count) => {
+      response.send(
+        `<p>Phonebook has started for ${count} people</p> <p>${new Date().toLocaleString()}</p>`
+      );
+    })
+    .catch((error) => {
+      response.status(500).send({ error: "Database error" });
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => response.json(person));
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => response.json(person))
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  // const id = request.params.id;
-  // persons = persons.filter((person) => person.id !== id);
-  // response.status(204).end();
   Person.findByIdAndDelete(request.params.id).then((result) => {
     response.status(204).end();
   });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    response.status(400).json({ error: "Name and Number are required" });
+  }
+
+  const person = { name, number };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      if (!updatedPerson) {
+        response.status(400).json({ error: "Person not found" });
+      }
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -80,19 +110,31 @@ app.post("/api/persons", (request, response) => {
   if (!number) {
     return response.status(400).json({ error: "Number Missing" });
   }
-  if (persons.some((person) => person.name === name)) {
-    return response.status(400).json({ error: "Name must be unique" });
-  }
 
-  const person = new Person({
-    name,
-    number,
-  });
+  Person.findOne({ name }).then((existingPerson) => {
+    if (existingPerson) {
+      Person.findByIdAndUpdate(existingPerson.id, { number }, { new: true })
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+    } else {
+      const person = new Person({
+        name,
+        number,
+      });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
+      person
+        .save()
+        .then((savedPerson) => {
+          response.json(savedPerson);
+        })
+        .catch((error) => next(error));
+    }
   });
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
